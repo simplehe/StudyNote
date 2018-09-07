cc6211a853d3b424c7cd993e1a10d7d080b28df4
simplehe <418417192@qq.com>
2018-09-07 15:13:13 +0800

    【提交】数据集

--- 机器学习实战/code/kNN.py
+++ 机器学习实战/code/kNN.py
@@ -1,5 +1,7 @@
 from numpy import *
 import operator
+import matplotlib
+import matplotlib.pyplot as plt
 
 
 def createDataSet():
@@ -23,5 +25,61 @@ def classify0(inX, dataSet, labels, k):
     return sortedClassCount[0][0]
 
 
-group, labels = createDataSet()
-print(classify0([0, 0], group, labels, 3))
+# group, labels = createDataSet()
+# print(classify0([0, 0], group, labels, 3))
+
+
+def file2matrix(filename):
+    fr = open(filename)
+    numberOfLines = len(fr.readlines())  # get the number of lines in the file
+    returnMat = zeros((numberOfLines, 3))  # prepare matrix to return
+    classLabelVector = []  # prepare labels return
+    fr = open(filename)
+    index = 0
+    for line in fr.readlines():
+        line = line.strip()
+        listFromLine = line.split('\t')
+        returnMat[index, :] = listFromLine[0:3]
+        classLabelVector.append(int(listFromLine[-1]))
+        index += 1
+    return returnMat, classLabelVector
+
+
+# datingDataMat, datingLabels = file2matrix('datingTestSet2.txt')
+#
+# fig = plt.figure()
+# ax = fig.add_subplot(111)
+# # ax.scatter(datingDataMat[:,1], datingDataMat[:,2])
+# print(15 * array(datingLabels))
+# ax.scatter(datingDataMat[:, 1], datingDataMat[:, 2], 15.0 * array(datingLabels), 15.0 * array(datingLabels))
+# ax.axis([-2, 25, -0.2, 2.0])
+# plt.xlabel('Percentage of Time Spent Playing Video Games')
+# plt.ylabel('Liters of Ice Cream Consumed Per Week')
+# plt.show()
+
+def autoNorm(dataSet):
+    minVals = dataSet.min(0)
+    maxVals = dataSet.max(0)
+    ranges = maxVals - minVals
+    normDataSet = zeros(shape(dataSet))
+    m = dataSet.shape[0]
+    normDataSet = dataSet - tile(minVals, (m, 1))
+    normDataSet = normDataSet / tile(ranges, (m, 1))
+    return normDataSet, ranges, minVals
+
+def datingClassTest():
+    hoRatio = 0.10
+    datingDataMat, datingLabels = file2matrix('datingTestSet2.txt')
+    normMat, ragnes, minVals = autoNorm(datingDataMat)
+    m = normMat.shape[0]
+    numTestVecs = int(m * hoRatio)
+    errorCount = 0.0
+
+    for i in range(numTestVecs):
+        classifierResult = classify0(normMat[i, :], normMat[numTestVecs:m, :], datingLabels[numTestVecs:m], 3)
+        print("the classifier came back with %d, ther real answer is: %d" % (classifierResult, datingLabels[i]))
+        if (classifierResult != datingLabels[i]):
+            errorCount += 1.0
+    print("the total error rate is: %f" % (errorCount / float(numTestVecs)))
+
+datingClassTest()
